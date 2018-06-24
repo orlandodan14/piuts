bac539ee26593583638d8332317a08be2afbb5ba
Orlando <orlando.dan14@gmail.com>
2018-06-24 16:18:22 -0400

    Sample App

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,81 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest  
+  
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "login with invalid information" do
+    #Visit the login path:
+    get login_path    
+    #Verify that the new sessions form renders properly:
+    assert_template 'sessions/new'    
+    #Post to the sessions path with an invalid params hash:
+    post login_path, params: { session: { email: "", password_digest: "" } }    
+    #Verify that the new sessions form gets re-rendered and that a flash message appears:
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    #Visit another page (such as the Home page):
+    get root_path    
+    #Verify that the flash message doesn’t appear on the new page
+    assert flash.empty?
+  end
+  
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "login with valid information" do
+    #Visit the login path.
+    get login_path
+    #Post valid information to the sessions path.
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'    
+    #Verify that the login link disappears.
+    assert_select "a[href=?]", login_path, count: 0
+    #Verify that a logout link appears.
+    assert_select "a[href=?]", logout_path
+    #Verify that a profile link appears.
+    assert_select "a[href=?]", user_path(@user)
+  end
+  
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+  
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
+end
