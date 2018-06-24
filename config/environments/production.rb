27ed224ff124275a46bd4bf4fb761d169c270802
Orlando <orlando.dan14@gmail.com>
2018-06-24 16:32:46 -0400

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'mysterious-mountain-47911.herokuapp.com'
+  host = 'piuts.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
